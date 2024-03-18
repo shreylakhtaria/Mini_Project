a4f6577eeff3d00009182e26357ef01ecbbb2689
Princelad <ladprince1112@gmail.com>
2024-03-18 12:39:52 +0530

    base

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/*
+This project is created by @shreylakhtaria and @Princelad
+*/
+
+/*
+An address book project.
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class contacts{
+string name;
+int contact;
+string e_mail;
+public:
+    contacts(){
+
+    }
+
+    void output(){
+
+    }
+
+    void search(){
+
+    }
+
+    void sort(){
+        
+    }
+};
+
+int main(){
+    return 0;
+}
