8e23576723852be6a7597823d985837d20547e97
Princelad <ladprince1112@gmail.com>
2024-04-17 18:47:43 +0530

    Main update, mainMenu function created, changed rot_13 cyphering to key cyphering and some basic program flow has been managed

--- main.cpp
+++ main.cpp
@@ -15,183 +15,266 @@
  
 #include <iostream>
 #include <unordered_map>
+
 using namespace std;
 
-class ROT13_numeric
-{
-private:
+class keyEncrypt {
     unordered_map<long long, long long> substitutionCipher;
     unordered_map<long long, long long> reverseCipher;
 
 public:
     // Constructor to initialize the substitution cipher mapping
-    ROT13_numeric()
-    {
-        for (long long i = 0; i < 10; ++i)
-        {
-            long long encrypted = (i + 13) % 10; // ROT13 shift
+    keyEncrypt(string key) {
+        
+        for (long long i = 0; i < 10; ++i) {
+            
+            long long encrypted = (i + stoi(key)) % 10; // ROT13 shift
             substitutionCipher[i] = encrypted;
             reverseCipher[encrypted] = i;
+
         }
+        
     }
 
     // Function to encrypt a number using ROT13 (Numeric) cipher
-    long long encrypt(long long num)
-    {
+    long long encrypt(long long num) {
+        
         string numStr = to_string(num);
         string encryptedNumStr = "";
-        for (char digit : numStr) // Ranged Loop.
-        {
+        
+        for (char digit : numStr) {// Ranged Loop.
+
             encryptedNumStr += to_string(substitutionCipher[digit - '0']);
         }
+
         return stoll(encryptedNumStr); // stoll is string to ll.
     }
 
     // Function to decrypt a number using ROT13 (Numeric) cipher
-    long long decrypt(long long num)
-    {
+    long long decrypt(long long num) {
+        
         string numStr = to_string(num);
         string decryptedNumStr = "";
-        for (char digit : numStr) // Ranged Loop.
-        {
+        
+        for (char digit : numStr) {// Ranged Loop.
+        
             decryptedNumStr += to_string(reverseCipher[digit - '0']);
         }
+        
         return stoll(decryptedNumStr); // stoll is string to long long.
+
     }
+
 };
 
 
-class Cipher_numeric 
-{
-private:
+class Cipher_numeric {
     unordered_map<long long, long long> substitutionCipher;
     unordered_map<long long, long long> reverseCipher; // Reverse mapping
 
 public:
     // Constructor to initialize the substitution cipher mapping
-    Cipher_numeric()
-    {
+    Cipher_numeric() {
         // Default substitution cipher mapping
         substitutionCipher = {
-            {0, 7}, {1, 3}, {2, 8}, {3, 1}, {4, 5}, {5, 6}, {6, 0}, {7, 2}, {8, 4}, {9, 9}};
+            {0, 7}, {1, 3}, {2, 8}, {3, 1}, {4, 5}, {5, 6}, {6, 0}, {7, 2}, {8, 4}, {9, 9} };
 
         // Create reverse mapping that will turn the number back to it's original value
-        for (const auto &pair : substitutionCipher)
-        {
+        for (const auto &pair : substitutionCipher) {
             reverseCipher[pair.second] = pair.first;
         }
+
     }
 
     // Function to encrypt a number using substitution cipher
-    long long encrypt(long long num)
-    {
+    long long encrypt(long long num) {
+        
         long long encryptedNum = 0;
         long long multiplier = 1;
         long long temp = num;
-        while (temp > 0)
-        {
+        
+        while (temp > 0) {
+            
             long long digit = temp % 10;
             encryptedNum += substitutionCipher[digit] * multiplier;
             multiplier *= 10;
             temp /= 10;
+
         }
+
         return encryptedNum;
+
     }
 
     // Function to decrypt a number using substitution cipher
-    long long decrypt(long long num)
-    {
+    long long decrypt(long long num) {
+        
         long long decryptedNum = 0;
         long long multiplier = 1;
         long long temp = num;
-        while (temp > 0)
-        {
+        
+        while (temp > 0) {
             long long digit = temp % 10;
             decryptedNum += reverseCipher[digit] * multiplier; // Use reverse mapping
             multiplier *= 10;
             temp /= 10;
         }
+
         return decryptedNum;
+
     }
+
 };
 
-int main()
-{
-    Cipher_numeric Cipher_numeric;
-    ROT13_numeric rot13;
-    long long num;
+int mainMenu() {
+
     int choice;
-
-
-    do
-    {
 
     cout << "Welcome To  Encryption/decryption: " << endl;
     cout << "*****************************************************************************" << endl;
     cout << "1: Encryption_(Substituion cipher): " << endl;
     cout << "2: decryption_(Substituion cipher): " << endl;
-    cout << "3: Encryption_(ROT_13 cipher): "      << endl;
-    cout << "4: Decryption_(ROT_13 cipher)"        << endl;
+    cout << "3: Encryption_(Key cipher)"      << endl;
+    cout << "4: Decryption_(Key cipher)"      << endl;
     cout << "5: exit: "                            << endl;
 
 
     cout << "Enter the choice " << endl;
     cin >> choice ;
 
-    switch (choice)
-    {
-    case 1:
-    {
-        cout << "enter the number:" << endl;
-        cin >> num;
-        long long encryptedNum = Cipher_numeric.encrypt(num);
-        cout << "Encrypted number: " << encryptedNum << endl;
-        break;
+    if(choice >= 1 && choice <= 5) {
+
+        return choice;
 
     }
+    else {
 
-    case 2:
-    {
-        cout << "enter the number:" << endl;
-        cin >> num;
-        long long decryptedNum = Cipher_numeric.decrypt(num);
-        cout << "Decrypted number: " << decryptedNum << endl;
-        break;
-    }
+        cout << "Enter proper choice.";
 
-    
-    case 3: 
-    {
-        cout << "Enter the number: " << endl;
-        cin >> num;
-        long long encryptedNum = rot13.encrypt(num);
-        cout << "Encrypted number: " <<  encryptedNum << endl;
-        break;
-    }
-
-    case 4:
-    {
-        cout << "enter the number: " << endl;
-        cin >> num;
-        long long decrytednum = rot13.decrypt(num);
-        cout << "Decrypted number: " << decrytednum << endl;
-        break;
-    }
-    case 5:
-    {
-        cout << "Exiting program..." << endl;
-        return 0;
+        mainMenu();
 
     }
-
-    default:
-        cout << "Invalid choice! Exiting program..." << endl;
-        return 1;
-    }
-    } while(choice != 5);
-
-
-
 
     return 0;
+   
+}
+
+int main() {
+
+    long long num;
+    string key;
+    
+    do {
+
+        switch (mainMenu())
+        {
+            
+            case 0: {
+
+                cout << "Error in mainmenu." << endl;
+
+                exit(1);
+
+            }
+            
+            case 1: {
+
+                Cipher_numeric *temp;
+
+                temp = new Cipher_numeric();
+                
+                cout << "enter the number:" << endl;
+                cin >> num;
+
+                num = temp -> encrypt (num);
+
+                cout << "Encrypted number: " << num << endl;
+
+                delete temp;
+
+                break;
+
+            }
+
+            case 2: {
+
+                Cipher_numeric *temp;
+
+                temp = new Cipher_numeric();
+                
+                cout << "enter the number:" << endl;
+                cin >> num;
+
+                num = temp -> decrypt(num);
+
+                cout << "Decrypted number: " << num << endl;
+
+                break;
+
+            }
+
+
+            case 3: {
+
+                cout << "Enter the number: " << endl;
+                cin >> num;
+
+                cout << "Enter the key:" << endl;
+                cin >> key;
+
+                keyEncrypt *temp;
+
+                temp = new keyEncrypt(key);
+
+                num = temp -> encrypt(num);
+
+                cout << "Encrypted number: " <<  num << endl;
+
+                break;
+
+            }
+
+            case 4: {
+
+                cout << "enter the number: " << endl;
+                cin >> num;
+
+                cout << "Enter the key:" << endl;
+                cin >> key;
+
+                keyEncrypt *temp;
+
+                temp = new keyEncrypt(key);
+
+                num = temp -> decrypt(num);
+
+                cout << "Decrypted number: " << num << endl;
+
+                break;
+
+            }
+
+            case 5: {
+
+                cout << "Exiting program..." << endl;
+
+                return 0;
+
+            }
+
+        }
+
+        cout << "Do you want to continue encrypting/decrypting? (Y)es or (n)o." << endl;
+        cin >> key;
+
+        if(key == "N" || key == "n") {
+
+            cout << "Exiting program..." << endl;
+
+            return 0; 
+
+        }
+
+    } while(true);
+
 }
